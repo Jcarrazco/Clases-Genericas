7f377647e7ccd459cf7633ef9ee6c394a7bbd9ef
Jcarrazco <48524432+Jcarrazco@users.noreply.github.com>
2019-03-21 17:19:39 -0600

    Add files via upload

---
+++ clasesgenericas/clasesgenericas/ClaseGenerica.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clasesgenericas
+{
+    public class printer <T> 
+    {
+        public string Print(T X)
+        {
+            string Nuevo = Convert.ToString(X);
+            return Nuevo;
+        }
+    }
+
+    public class swper<T> where T : struct
+    {
+        public void swap ( T X, T X2)
+        {
+            T temp;
+            temp = X2;
+            X2 = X;
+            X = temp;
+            Console.WriteLine(X + " " + X2);
+        }
+    }
+}
---
+++ clasesgenericas/clasesgenericas/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clasesgenericas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 50,  x2 = 20;
+            printer<string> prt = new printer<string>();
+
+            Console.WriteLine("Escriba un string para enviar a la clase");
+            string n = prt.Print(Console.ReadLine());
+            Console.WriteLine(n);
+
+            swper<int> sw = new swper<int>();
+            sw.swap(x1,x2);
+            Console.ReadKey();
+
+            
+        }
+    }
+}
